daafef7e93df0de8d18cd96c715731b20464f12c
gilbmporto <gilbmp@gmail.com>
2025-03-13 11:49:08 -0300

    Initialized money management app with Next.js 15 and other tech stacks

---
+++ src/components/UserDropdown.tsx
@@ -0,0 +1,20 @@
+"use client"
+
+import { UserButton } from "@clerk/nextjs"
+
+function UserDropdown() {
+  return (
+    <UserButton
+      showName
+      appearance={{
+        elements: {
+          userButtonOuterIdentifier: {
+            color: "white",
+          },
+        },
+      }}
+    />
+  )
+}
+
+export default UserDropdown
