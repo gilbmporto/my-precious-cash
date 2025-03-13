1719efe913c7f275e995894baacfc0e65ccf9065
gilbmporto <gilbmp@gmail.com>
2025-03-13 15:01:20 -0300

    Added shadcn UI elements | Started development of '/dashboard/transactions/new' route

---
+++ src/app/dashboard/transactions/new/page.tsx
@@ -0,0 +1,46 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import Link from "next/link"
+import React from "react"
+
+function NewTransactionPage() {
+  return (
+    <div className="max-w-screen-xl mx-auto py-10 px-5">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/transactions">Transactions</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>New Transaction</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+
+      <Card className="mt-4 max-w-screen-md">
+        <CardHeader>
+          <CardTitle>New Transaction</CardTitle>
+        </CardHeader>
+        <CardContent>New Transaction Form</CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default NewTransactionPage
